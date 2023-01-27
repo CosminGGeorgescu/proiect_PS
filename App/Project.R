4b61235021ab2b59454314867160619bcb186f27
Tanase Flavian <flaviantanase30@gmail.com>
2023-01-27 16:13:13 +0200

    deocamdata atat

--- App/Project.R
+++ App/Project.R
@@ -17,7 +17,8 @@ ui <- fluidPage(
                    sidebarLayout(
                      sidebarPanel(
                        
-                       numericInput("n", "Numarul de experimente : ", value = 10, min = 1),
+                       numericInput("n", "Numarul de experimente : ", value = 10, min = 1, max = 1000),
+                       numericInput("k", "Numarul de realizari : ", value = 5, min = 1, max = 1000),
                        sliderInput("p", "Probabilitatea de realizare :", min = 0, max = 1, value = 0.5, step = 0.01),
                        radioButtons("func", "Selectati Histograma", choices = c("Functia de repartitie", 
                                                                                 "Functia de masa")
@@ -30,23 +31,46 @@ ui <- fluidPage(
                      
                    
                    mainPanel(
+                     conditionalPanel(condition = "input.func == 'Functia de repartitie'",
                      plotOutput("cdf")
+                     ),
+                     conditionalPanel(condition = "input.func == 'Functia de masa'",
+                                      plotOutput("pmf")
+                                      
                    )
                  )
   )
+)
 )
 
 
 
 server <- function(input, output) {
   output$cdf <- renderPlot({
-    x <- 0:input$n
-    y <- dbinom(x, size = input$n, prob = input$p)
-    barplot(y, names.arg = x, 
-            xlab = "Numarul de succese", 
-            ylab = "Probabilitatea", 
-            main = "Functia de masa a distributiei binomiale")
-     })
+    n <- input$n
+    p <- input$p
+    k <- input$k
+    x <- 0:n
+    cdf <- pbinom(x, size = n, prob = p)
+    plot(x, cdf, type = "l", lwd = 2, 
+         xlab = "Number of Successes", 
+         ylab = "Cumulative Probability",
+         main = paste("Binomial CDF (n =", n, ", p =", p, ")")
+    )
+    abline(v = k, lty = 2)     })
+  output$pmf <- renderPlot({
+    n <- input$n
+    p <- input$p
+    k <- input$k
+    x <- 0:n
+    pmf <- dbinom(x, size = n, prob = p)
+    plot(x, pmf, type = "h", lwd = 2, 
+         xlab = "Number of Successes", 
+         ylab = "Probability",
+         main = paste("Binomial PMF (n =", n, ", p =", p, ")")
+    )
+    abline(v = k, lty = 2)
+  })
 }
 
 # Run the app ----
