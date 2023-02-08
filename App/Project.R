bbbc6d7d9ab77ecf91191f0ecf1d961cfc945782
CosminGGeorgescu <cosmingg2013@gmail.com>
2023-02-08 16:51:46 +0200

    Student done

--- App/Project.R
+++ App/Project.R
@@ -10,66 +10,71 @@ ui <- fluidPage(
                                                            "Negativ Binomiala",
                                                            "Poisson",
                                                            "Geometrica",
-                                                           "Hipergeometrica")
-  ),
-  
-  conditionalPanel(condition = "input.repart == 'Binomiala'",
-                   sidebarLayout(
-                     sidebarPanel(
-                       
-                       numericInput("n", "Numarul de experimente : ", value = 10, min = 1, max = 1000),
-                       numericInput("k", "Numarul de realizari : ", value = 5, min = 1, max = 1000),
-                       sliderInput("p", "Probabilitatea de realizare :", min = 0, max = 1, value = 0.5, step = 0.01),
-                       radioButtons("func", "Selectati Histograma", choices = c("Functia de repartitie", 
-                                                                                "Functia de masa")
-                       ),
-                       numericInput("a", "a = ", value = 10),
-                       numericInput("b", "b = ", value = 10)
-                       
-                       
-                     ),
+                                                           "Hipergeometrica",
+                                                           "Student")),
+  conditionalPanel(condition = "input.repart == 'Student'",
+                 sidebarLayout(
+                   sidebarPanel(
                      
+                     sliderInput("student_b", "Bound", min = 10, max = 50, value = 20),
+                     sliderInput("student_dof", "Degrees of freedom :", min = 0, max = 10, value = 1, step = 0.1),
+                     sliderInput("student_c", "Center", min = 0, max = 25, value = 0),
+                     
+                     numericInput("student_a", "a = ", value = 10),
+                     numericInput("student_b", "b = ", value = 10)
+                     
+                     
+                     
+                   ),
+                   
                    
                    mainPanel(
-                     conditionalPanel(condition = "input.func == 'Functia de repartitie'",
-                     plotOutput("cdf")
-                     ),
-                     conditionalPanel(condition = "input.func == 'Functia de masa'",
-                                      plotOutput("pmf")
-                                      
+          
+                     plotOutput("student_dens"),
+                     plotOutput("student_rep"),
+                     
                    )
                  )
-  )
-)
+),
 )
 
+plot_poligon <- function(x, a, b, fun){
+  polygon(c(0, x[x<=a],a), 
+          c(0, fun[x<=a],0), density = 15, angle = 135 , lty = 5, lwd = 2,
+          col="black")
+  polygon(c(a, x[x>=a & x<=b],b), 
+          c(0, fun[x>=a & x<=b],0), density = 15, lty = 2, lwd = 2,
+          col="black")
+  polygon(c(b, x[x>=b], x[length(x)]), 
+          c(0, fun[x>=b],0), density = 15, angle = 135, lwd = 2,
+          col="black")
+}
 
+plot_continua <- function(x, fun, xlab, ylab, main){
+  plot(x, fun, type = "l", lty=2, lwd=2,
+       xlab=xlab, ylab=ylab, main=main)
+}
+
+plot_bars <- function(x, fun, a, b, l){
+  barplot(names.arg = x, height = fun, xlab = "x", ylab = "Fx(x)", 
+          main = "P(x<=a) P(a<=x<=b) P(x>=b)", 
+          col = c(rep("blue", a+1), 
+                  rep("gray", b - a),
+                  rep("purple", l) ) )
+}
 
 server <- function(input, output) {
-  output$cdf <- renderPlot({
-    n <- input$n
-    p <- input$p
-    k <- input$k
-    x <- 0:n
-    cdf <- pbinom(x, size = n, prob = p)
-    plot(x, cdf, type = "l", lwd = 2, 
-         xlab = "Number of Successes", 
-         ylab = "Cumulative Probability",
-         main = paste("Binomial CDF (n =", n, ", p =", p, ")")
-    )
-    abline(v = k, lty = 2)     })
-  output$pmf <- renderPlot({
-    n <- input$n
-    p <- input$p
-    k <- input$k
-    x <- 0:n
-    pmf <- dbinom(x, size = n, prob = p)
-    plot(x, pmf, type = "h", lwd = 2, 
-         xlab = "Number of Successes", 
-         ylab = "Probability",
-         main = paste("Binomial PMF (n =", n, ", p =", p, ")")
-    )
-    abline(v = k, lty = 2)
+  output$student_dens <- renderPlot({
+    #x <- seq(input$student_b / -2, input$student_b, by=0.1)
+    #plot(x, dt(x, df=input$student_dof, ncp=input$student_c), type="l")
+    plot(function(x) dt(x, df=input$student_dof, ncp=input$student_c), input$student_b / -2, input$student_b,
+         xlab = "X", ylab = "fx(x)", main = "Densitatea repartitiei student")
+  })
+  output$student_rep <- renderPlot({
+    plot(function(x) pt(x, df=input$student_dof, ncp=input$student_c), input$student_b / -2, input$student_b,
+         xlab = "X", ylab = "Fx(x)", main = "Functia de repartitie student && P(x<=a) P(a<=x<=b) P(x>=b)")
+    x <- seq(input$student_b / -2, input$student_b, by=0.1)
+    plot_poligon(x = x, a = input$student_a, b = input$student_b, fun = pt(x, df=input$student_dof, ncp=input$student_c))
   })
 }
 
